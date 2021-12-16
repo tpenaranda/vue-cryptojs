9493e5e7950af161e6f897ee04975e67f74339d3
Tate Peñaranda <tate@tpenaranda.com>
2021-12-16 02:44:55 -0300

    Support for Vue3

--- dist/vue-cryptojs.es5.js
+++ dist/vue-cryptojs.es5.js
@@ -12,19 +12,31 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 /* eslint-disable */
 var VueCryptojs = {
   install: function install(Vue, options) {
-    Vue.CryptoJS = _cryptoJs["default"];
-    Object.defineProperties(Vue.prototype, {
-      CryptoJS: {
-        get: function get() {
-          return _cryptoJs["default"];
+    Vue.CryptoJS = _cryptoJs["default"]; // VueJS 2
+
+    if (Vue.prototype) {
+      Object.defineProperties(Vue.prototype, {
+        $CryptoJS: {
+          get: function get() {
+            return _cryptoJs["default"];
+          }
+        },
+        CryptoJS: {
+          get: function get() {
+            return _cryptoJs["default"];
+          }
         }
-      },
-      $CryptoJS: {
-        get: function get() {
-          return _cryptoJs["default"];
-        }
-      }
-    });
+      });
+    } // VueJS 3
+
+
+    if (Vue.config && Vue.config.globalProperties) {
+      Vue.config.globalProperties.$CryptoJS = _cryptoJs["default"];
+    }
+
+    if (Vue.provide && typeof Vue.provide === 'function') {
+      Vue.provide('cryptojs', _cryptoJs["default"]);
+    }
 
     if (typeof window !== 'undefined' && window.Vue) {
       window.Vue.use(VueCryptojs);
--- src/index.mjs
+++ src/index.mjs
@@ -4,10 +4,26 @@ import cryptojs from 'crypto-js'
 const VueCryptojs = {
   install (Vue, options) {
     Vue.CryptoJS = cryptojs
-    Object.defineProperties(Vue.prototype, {
-      CryptoJS: { get() { return cryptojs } },
-      $CryptoJS: { get() { return cryptojs } }
-    })
+
+    // VueJS 2
+
+    if (Vue.prototype) {
+      Object.defineProperties(Vue.prototype, {
+        $CryptoJS: { get() { return cryptojs } },
+        CryptoJS: { get() { return cryptojs } }
+      })
+    }
+
+    // VueJS 3
+
+    if (Vue.config && Vue.config.globalProperties) {
+      Vue.config.globalProperties.$CryptoJS = cryptojs
+    }
+
+    if (Vue.provide && typeof Vue.provide === 'function') {
+      Vue.provide('cryptojs', cryptojs)
+    }
+
     if (typeof window !== 'undefined' && window.Vue) {
       window.Vue.use(VueCryptojs)
     }
